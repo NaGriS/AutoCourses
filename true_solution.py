363bd92d918ed5aa4b5e594feb74375f305004d2
egormalysev <egorpw@yandex.ru>
2019-02-06 23:45:05 +0300

    python power

---
+++ true_solution.py
@@ -0,0 +1,10 @@
+
+
+if __name__ == '__main__':
+    str1 = input("Input first string: ")
+    str2 = input("Input second string: ")
+
+    res = str1 + str2
+    print(f'concat: {res}')
+    print(f'2 in 1: {str1 in str2}')
+    print(f'2 len: {len(str2)}')
